c402186a662f98b5844b0a1bace0be42cb188631
wesleisantos071 <wesleisantos071@gmail.com>
2024-02-11 18:18:58 +0100

    Refactored package name

--- src/main/java/com/theweslei/samples/scheduling/DemoApplication.java
+++ src/main/java/com/theweslei/samples/scheduling/DemoApplication.java
@@ -1,8 +1,7 @@
-package com.example.demo;
+package com.theweslei.samples.scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
